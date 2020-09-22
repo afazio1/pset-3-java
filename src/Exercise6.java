1c386a22356fdef0e035a55ec952209bbe5b5bad
Alexa <alexa.fazio04@gmail.com>
2020-09-22 14:35:25 -0400

    finished exercise 6

---
+++ src/Exercise6.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        decimalFormat.setGroupingUsed(true);
+        decimalFormat.setGroupingSize(3);
+
+        System.out.print("Wage      : ");
+        double hourlyWage = in.nextDouble();
+        System.out.print("Monday    : ");
+        double mondayHours = in.nextDouble();
+        System.out.print("Tuesday   : ");
+        double tuesdayHours = in.nextDouble();
+        System.out.print("Wednesday : ");
+        double wednesdayHours = in.nextDouble();
+        System.out.print("Thursday  : ");
+        double thursdayHours = in.nextDouble();
+        System.out.print("Friday    : ");
+        double fridayHours = in.nextDouble();
+        System.out.print("Saturday  : ");
+        double saturdayHours = in.nextDouble();
+        System.out.print("Sunday    : ");
+        double sundayHours = in.nextDouble();
+
+        double totalHours = mondayHours + tuesdayHours + wednesdayHours
+                + thursdayHours + fridayHours + saturdayHours + sundayHours;
+
+        double weeklySalary = totalHours * hourlyWage;
+        System.out.println("\n$" + decimalFormat.format(weeklySalary) + ".");
+    }
+}
