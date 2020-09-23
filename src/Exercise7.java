aed3a8bfc8033b208c7f30121758585933a88e90
Alexa <alexa.fazio04@gmail.com>
2020-09-22 21:59:51 -0400

    finished exercises 7 and 8

---
+++ src/Exercise7.java
@@ -0,0 +1,28 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+
+        decimalFormat.setGroupingUsed(true);
+        decimalFormat.setGroupingSize(3);
+
+        System.out.print("Salary  : ");
+        double salary = in.nextDouble();
+        System.out.print("401(k)  : ");
+        double tax401k = in.nextDouble() / 100;
+        System.out.print("Federal : ");
+        double federal = in.nextDouble() / 100;
+        System.out.print("State   : ");
+        double state = in.nextDouble() / 100;
+        double finalSalary = salary - (salary * tax401k);
+        finalSalary = finalSalary - (finalSalary * (federal + state));
+
+        double biWeeklySalary = finalSalary / 24;
+
+        System.out.print("\n$" + decimalFormat.format(biWeeklySalary) + ".");
+
+    }
+}
---
+++ src/Exercise8.java
@@ -0,0 +1,23 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("0");
+
+        System.out.print("Students : ");
+        int students = in.nextInt();
+        System.out.print("Teachers : ");
+        int teachers = in.nextInt();
+        System.out.print("Capacity : ");
+        int capacity = in.nextInt();
+
+        int totalPassengers = students + teachers;
+        int buses = totalPassengers / capacity;
+        int overflow = totalPassengers % capacity;
+
+        System.out.println("\n" + "Buses required      : " + decimalFormat.format(buses + 1));
+        System.out.print("Overflow passengers : " + overflow);
+    }
+}
